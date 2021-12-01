b5bf210a6848d3c7f7de3ada5649ebe6f0656d90
arina <49390842+arinaskh@users.noreply.github.com>
2021-12-01 13:44:11 +0330

    Add files via upload

---
+++ MATLAB exercises/week4/exercise2.m
@@ -0,0 +1,3 @@
+n=input('please enter the first number:');
+m=input('please enter the second number :');
+sprintf('the gcdl is : %d , gcd(s,m))
---
+++ MATLAB exercises/week4/exercise3.m
@@ -0,0 +1,25 @@
+%second method in MATLAB
+syms x
+f(x)=x+cos(x);
+%f(-1)=-0.4597 , f(0) = 1 , f(-1)*f(0) <1
+x1 = -1;
+xu=0;
+%some random iteratie
+N=5;
+%we will check if we have roots
+if(f(x1) * f(xu) >0)
+disp('there is no root in this range');
+else
+for i=1:N
+  xr=(xl*f(xu)-xu*f(xl))/(f(xu)-f(xl));
+  
+  if f(xl)*f(xr) < 0
+    xu = xr:
+  end
+  if f(xu)*f(xr) <0
+    xl=xr:
+    enddisp(double(xr));
+    end
+  endif
+disp('root is :0
+vpa(solve(f,0),3)
